82e0f06bbe8db4fa3ed22062c22ebf161a5341fe
Matheus Branco Borella <dark.ryu.550@gmail.com>
2024-06-25 16:04:01 -0300

    Fix lints and small issues

--- pwndbg/commands/__init__.py
+++ pwndbg/commands/__init__.py
@@ -677,6 +677,7 @@ def HexOrAddressExpr(s: str) -> int:
 def load_commands() -> None:
     # pylint: disable=import-outside-toplevel
     import pwndbg.dbg
+
     if pwndbg.dbg.is_gdblib_available():
         import pwndbg.commands.ai
         import pwndbg.commands.argv
