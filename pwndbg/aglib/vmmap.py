00ca81bc207b920de4c35ce9b9ef3eed0a3e3b12
Matheus Branco Borella <dark.ryu.550@gmail.com>
2024-08-16 20:54:21 -0300

    Add caching to `pwndbg.aglib.vmmap`

--- pwndbg/aglib/vmmap.py
+++ pwndbg/aglib/vmmap.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Tuple
 
 import pwndbg
+import pwndbg.lib.cache
 import pwndbg.lib.memory
 
 if pwndbg.dbg.is_gdblib_available():
@@ -17,6 +18,7 @@ if pwndbg.dbg.is_gdblib_available():
     import pwndbg.gdblib.vmmap
 
 
+@pwndbg.lib.cache.cache_until("start", "stop")
 def get() -> Tuple[pwndbg.lib.memory.Page, ...]:
     if pwndbg.dbg.is_gdblib_available():
         return pwndbg.gdblib.vmmap.get()
@@ -24,6 +26,7 @@ def get() -> Tuple[pwndbg.lib.memory.Page, ...]:
     return tuple(pwndbg.dbg.selected_inferior().vmmap().ranges())
 
 
+@pwndbg.lib.cache.cache_until("start", "stop")
 def find(address: int | pwndbg.dbg_mod.Value | None) -> pwndbg.lib.memory.Page | None:
     if address is None:
         return None
